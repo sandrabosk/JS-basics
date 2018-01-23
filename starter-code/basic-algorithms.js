bf50d7e53ac3e4490b5952a6b7029bf341eb0da0
sandrabosk <sandraboskovic@hotmail.com>
2018-01-23 09:07:48 -0500

    done

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -54,3 +54,34 @@ console.log(n);
 // Returns -1 if str1 is sorted before str2
 // Returns 0 if the two strings are equal
 // Returns 1 if str1 is sorted after str2
+
+
+//Ask the user for a new string and check if it's a Palindrome.
+var str = prompt("Let's check the palindrome. Give me a word:");
+
+function checkPalindrome(str) {
+  return (
+    str ==
+    str
+      .split("")
+      .reverse()
+      .join("")
+  );
+}
+var value = checkPalindrome(str);
+// console.log("value:",value)
+
+if (value === true) {
+  console.log("The word is palindrome.");
+} else {
+  console.log("The word isn't palindrome.");
+}
+
+// Generate 3 parragraphs.Store the text in a String
+// Make your program count the number of words in the string
+// Make your program count the number of times the latin word et appears
+var string = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like). et et et "
+
+console.log(string.split(' ').length);
+var count = (string.match(/et/g) || []).length;
+console.log(count);
